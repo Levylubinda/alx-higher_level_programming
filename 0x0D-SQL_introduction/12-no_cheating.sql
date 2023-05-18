093335193d7da54b5768e45b1acd5c9b2790604a
Levylubinda <lubasi223@gmail.com>
2023-05-18 09:39:09 -0700

    alx-higher_level_programming

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,6 +1,3 @@
--- Lists all records in the table second_table with a score >= 10 in my MySQL server.
--- Records are ordered by descending score.
-SELECT `score`, `name`
-FROM `second_table`
-WHERE `score` >= 10
-ORDER BY `score` DESC;
+-- updates a value in the database table
+-- updates bobs score to 10
+UPDATE second_table SET `score` = 10 WHERE `name` = "Bob";
