e8fcdb8c15cce856fa02ccffa93ef4badd1c7e48
Levylubinda <lubasi223@gmail.com>
2023-05-18 09:33:17 -0700

    alx-higher_level_programming

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,3 @@
+-- avg temp F of city top 3
+-- displays the average temp of a city ordered by desc
+SELECT `city`, AVG(`value`) AS "avg_temp" FROM temperatures WHERE `month` = 7 OR `month` = 8 GROUP BY `city` ORDER BY AVG(`value`) DESC LIMIT 3;
